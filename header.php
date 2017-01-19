4f3b4edab92db85abb3f9c5afae9b958fac056ff
Juraj Kiss <kiss@lbstudio.sk>
2017-01-19 09:39:58 +0100

    header color fix, remove Montserrat font as it is no longer used

--- header.php
+++ header.php
@@ -23,7 +23,6 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
 <?php wp_head(); ?>
 </head>
 
